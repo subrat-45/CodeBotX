b758d62f5cb338f65337ec63df3d4835fa78fed2
TEC-Subrat <subrat.kumar22042005@gmail.com>
2025-11-06 00:10:42 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,7 @@ dbConneect();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://codebotx-frontend.onrender.com",
   credentials: true
 }));
 app.use(morgan('dev'));
