7681ac92302942b4f0575b4856bca01212faf0a4
TEC-Subrat <subrat.kumar22042005@gmail.com>
2025-11-05 23:52:47 +0530

    Update axios.js

--- Frontend/src/Config/axios.js
+++ Frontend/src/Config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL : "https://codebotx-backend.onrender.com",
+    baseURL : import.meta.env.VITE_API_URL,
     headers : {
         "Authorization" : `Bearer ${localStorage.getItem("token")}`
     }
