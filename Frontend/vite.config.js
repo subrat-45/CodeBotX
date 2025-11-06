9bca7bd82c8dcb4a4db320cd76c7c78d3f865e54
TEC-Subrat <subrat.kumar22042005@gmail.com>
2025-11-06 09:03:00 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,9 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    sourcemap: false
+  },
   server: {
     headers: {
       "Cross-Origin-Embedder-Policy": "require-corp",
